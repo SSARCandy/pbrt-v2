4173fda5644f224d38538d8dc26ed405bf1e5e19
ssarcandy <ssarcandy@gmail.com>
2016-10-03 02:35:44 +0800

    half way....

--- src/shapes/heightfieldImproved.cpp
+++ src/shapes/heightfieldImproved.cpp
@@ -1,9 +1,11 @@
 
 // shapes/heightfieldImproved.cpp*
+
 #include "stdafx.h"
 #include "shapes/heightfieldImproved.h"
 #include "shapes/trianglemesh.h"
 #include "paramset.h"
+//#include "core/primitive.h"
 
 // heightfieldImproved Method Definitions
 HeightfieldImproved::HeightfieldImproved(const Transform *o2w, const Transform *w2o,
@@ -13,6 +15,7 @@ HeightfieldImproved::HeightfieldImproved(const Transform *o2w, const Transform *
     ny = y;
     z = new float[nx*ny];
     memcpy(z, zs, nx*ny*sizeof(float));
+
 }
 
 
@@ -27,7 +30,16 @@ BBox HeightfieldImproved::ObjectBound() const {
         if (z[i] < minz) minz = z[i];
         if (z[i] > maxz) maxz = z[i];
     }
-    return BBox(Point(0,0,minz), Point(1,1,maxz));
+	return BBox(Point(0, 0, minz), Point(1, 1, maxz));
+}
+
+bool HeightfieldImproved::VoxelIntersector(const Ray &ray, int i, int j, Intersection *in) const {
+	Point TL(voxelToPos(i, 0),     voxelToPos(j, 1),     getZ(i, j));
+	Point TR(voxelToPos(i + 1, 0), voxelToPos(j, 1),     getZ(i + 1, j));
+	Point BR(voxelToPos(i + 1, 0), voxelToPos(j + 1, 1), getZ(i + 1, j + 1));
+	Point BL(voxelToPos(i, 0),     voxelToPos(j + 1, 1), getZ(i, j + 1));
+
+	return false;
 }
 
 
@@ -46,10 +58,78 @@ HeightfieldImproved *CreateHeightfieldImprovedShape(const Transform *o2w, const 
     return new HeightfieldImproved(o2w, w2o, reverseOrientation, nu, nv, Pz);
 }
 
-bool HeightfieldImproved::Intersect(const Ray &ray, float *tHit, float *rayEpsilon, DifferentialGeometry *dg) const {
+bool HeightfieldImproved::Intersect(const Ray &r, float *tHit, float *rayEpsilon, DifferentialGeometry *dg) const {
+
+	return false;
+	// Transform _Ray_ to object space
+	Ray ray;
+	(*WorldToObject)(r, &ray);
+
+	// Check if ray intersect with BBox, and get intersection between ray & bbox
+	float rayT;
+	BBox bbox = ObjectBound();
+	if (bbox.Inside(ray(ray.mint))) {
+		rayT = ray.mint;
+	} else if (!bbox.IntersectP(ray, &rayT)) {
+		return false;
+	}
+
+	// move t (unit) from ray.o
+	Point gridIntersect = ray(rayT);
+
+
+	// Set up 3D DDA for ray
+	float NextCrossingT[3], DeltaT[3];
+	int Step[3], Out[3], Pos[3];
+	for (int axis = 0; axis < 3; ++axis) {
+		// Compute current voxel for axis
+		Pos[axis] = posToVoxel(gridIntersect, axis);
+		if (ray.d[axis] >= 0) {
+			// Handle ray with positive direction for voxel stepping
+			NextCrossingT[axis] = rayT + (voxelToPos(Pos[axis] + 1, axis) - gridIntersect[axis]) / ray.d[axis];
+			DeltaT[axis] = 1.0 / ray.d[axis];
+			Step[axis] = 1;
+			Out[axis] = nVoxels[axis];
+		} else {
+			// Handle ray with negative direction for voxel stepping
+			NextCrossingT[axis] = rayT + (voxelToPos(Pos[axis], axis) - gridIntersect[axis]) / ray.d[axis];
+			DeltaT[axis] = -1.0 / ray.d[axis];
+			Step[axis] = -1;
+			Out[axis] = -1;
+		}
+	}
+
+	// Walk ray through voxel grid
+	bool hitSomething = false;
+	Intersection intersection;
+	for (;;) {
+		int i = Pos[0], j = Pos[1];
+		hitSomething = VoxelIntersector(ray, i, j, &intersection);
+
+		// Advance to next voxel
+
+		// Find _stepAxis_ for stepping to next voxel
+		int bits = ((NextCrossingT[0] < NextCrossingT[1]) << 2) +
+			       ((NextCrossingT[0] < NextCrossingT[2]) << 1) +
+			       ((NextCrossingT[1] < NextCrossingT[2]));
+		const int cmpToAxis[8] = { 2, 1, 2, 1, 2, 2, 0, 0 };
+		int stepAxis = cmpToAxis[bits];
+		if (ray.maxt < NextCrossingT[stepAxis])
+			break;
+		Pos[stepAxis] += Step[stepAxis];
+		if (Pos[stepAxis] == Out[stepAxis])
+			break;
+		NextCrossingT[stepAxis] += DeltaT[stepAxis];
+	}
+
+	if (!hitSomething) return false;
+
+	// fill-in the intersection properities
+
+
 	return false;
 }
 
-bool HeightfieldImproved::IntersectP(const Ray &ray) const {
+bool HeightfieldImproved::IntersectP(const Ray &r) const {
 	return false;
 }
--- src/shapes/heightfieldimproved.h
+++ src/shapes/heightfieldimproved.h
@@ -16,13 +16,27 @@ public:
     HeightfieldImproved(const Transform *o2, const Transform *w2o, bool ro, int nu, int nv, const float *zs);
     ~HeightfieldImproved();
     bool CanIntersect() const;
-	bool Intersect(const Ray & ray, float * tHit, float * rayEpsilon, DifferentialGeometry * dg) const;
-	bool IntersectP(const Ray & ray) const;
+	bool Intersect(const Ray &r, float *tHit, float *rayEpsilon, DifferentialGeometry *dg) const;
+	bool IntersectP(const Ray &r) const;
     BBox ObjectBound() const;
 private:
+	inline int posToVoxel(const Point &P, int axis) const {
+		if (axis == 2) return 0;
+		int v = Float2Int(P[axis] * nVoxels[axis]);
+		return Clamp(v, 0, nVoxels[axis]);
+	}
+	inline float voxelToPos(int p, int axis) const {
+		return p / (float)(nVoxels[axis]);
+	}
+	inline float getZ(int x, int y) const {
+		return z[y*nx + x];
+	}
+	bool VoxelIntersector(const Ray &ray, int i, int j, Intersection *in) const;
+
     // heightfieldImproved Private Data
     float *z;
     int nx, ny;
+	int nVoxels[3]; // [nx-1, ny-1, 1]
 };
 
 
