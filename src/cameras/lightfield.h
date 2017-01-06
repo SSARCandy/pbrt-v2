b80ea015a0cac9bbc7cb050ac0443503153aa73c
ssarcandy <ssarcandy@gmail.com>
2017-01-06 22:28:24 +0800

    parse lens specfile

--- src/cameras/lightfield.h
+++ src/cameras/lightfield.h
@@ -33,51 +33,38 @@ struct LensletStruct {
     
 };
 
-// Example representation of an autofocus zone.
-class AfZone {
-	public:
-	  // from the data file
-	  float left, right;
-	  float top, bottom;
-	  int xres,yres;
-};
+
 class LFCamera : public Camera {
 public:
-   LFCamera(const AnimatedTransform &cam2world,
-      float hither, float yon, float sopen,
-      float sclose, float lensletDistance, float aperture_diameter,
-      const string &specfile,
-      const string &lensletSpec,
-	  const string &autofocusfile,
-      float filmdiag,
-	  Film *film);
-   ~LFCamera();
-   float GenerateRay(const CameraSample &sample, Ray *) const;
-    bool surfaceIntercept(SurfaceSpec ss, Ray r, DifferentialGeometry &dg) const;
-   void  AutoFocus(Renderer * renderer, const Scene * scene, Sample * origSample);
-   void  ParseAfZones(const string& filename);
-    float TestAutoFocusDepth(Renderer * renderer, const Scene * scene, Sample * origSample, char* output);
-    LensletStruct lensletArray;
-    float stored_back_lens_a;
-    float calculatePrincipalPlane();
+	LFCamera(const AnimatedTransform &cam2world,
+		float hither, float yon, float sopen,
+		float sclose, float lensletDistance, float aperture_diameter,
+		const string &specfile,
+		const string &lensletSpec,
+		const string &autofocusfile,
+		float filmdiag,
+		Film *film);
+	~LFCamera();
+	float GenerateRay(const CameraSample &sample, Ray *) const;
+	bool surfaceIntercept(SurfaceSpec ss, Ray r, DifferentialGeometry &dg) const;
+	LensletStruct lensletArray;
+	float stored_back_lens_a;
+	float calculatePrincipalPlane();
 
 private:
-   bool  autofocus;
-   vector<AfZone> afZones;
-   float ShutterOpen;
-   float ShutterClose;
-   Film * film;
-    
-    vector<SurfaceSpec> lensSpec;
-    float mylensletdistance;
-    float myaperture_diameter;
-    float myfilmdiag;
-    float myfilmy;
-    float myfilmx;
-    int failures;
-    int successes;
-    
-    
+	bool  autofocus;
+	float ShutterOpen;
+	float ShutterClose;
+	Film * film;
+
+	vector<SurfaceSpec> lensSpec;
+	float mylensletdistance;
+	float myaperture_diameter;
+	float myfilmdiag;
+	float myfilmy;
+	float myfilmx;
+	int failures;
+	int successes;
 };
 
 LFCamera *CreateLFCamera(const ParamSet &params,
